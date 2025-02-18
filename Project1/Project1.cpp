0385ebea62a1aed9742cf8b2defbe9e5643592ce
AJ-18 <arjun.suresh1@marist.edu>
2025-02-18 01:16:49 -0500

    Start Project1

---
+++ Project1/Project1.cpp
@@ -0,0 +1,11 @@
+// Project1.cpp: Arjun Suresh - Parallel Merge Sort 
+
+#include <iostream>
+#include <fstream>
+#include <thread>
+#include <mutex>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
